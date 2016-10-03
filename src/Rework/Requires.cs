35f6014d55ca9178d6f1de46b947d698abde8965
Luke Warren <lukejkw@gmail.com>
2016-10-03 09:57:14 +0200

    Add Requires class with handy methods for validating params

---
+++ src/Rework/Requires.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Rework
+{
+    /// <summary>
+    /// A set of handy helper functions for checking if objects are null.
+    /// </summary>
+    public class Requires
+    {
+        /// <summary>
+        /// Checks if the object is null. If it is, an exception is thrown
+        /// </summary>
+        /// <param name="obj">The object to test</param>
+        public static void NotNull(object obj)
+        {
+            if (obj == null)
+                throw new ArgumentNullException();
+        }
+
+        /// <summary>
+        /// Checks if a Collection is Null or contains no items. Throws either a ArgumentNullException or an ArgumentException
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="items"></param>
+        public static void NotNullOrEmpty<T>(IEnumerable<T> items)
+        {
+            if (items == null)
+                throw new ArgumentNullException();
+            if (items.Count() <= 0)
+                throw new ArgumentException();
+        }
+
+        /// <summary>
+        /// Checks if a string is Null, Empty, Or whitespace or throws an argument null exception
+        /// </summary>
+        /// <param name="str">The string to test</param>
+        public static void NotNullOrEmptyOrWhitespace(string str)
+        {
+            if (String.IsNullOrEmpty(str) || String.IsNullOrWhiteSpace(str))
+                throw new ArgumentNullException();
+        }
+    }
+}
---
+++ src/UnitTests/RequiresTests.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+using Rework;
+using System;
+using System.Collections.Generic;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class RequiresTests
+    {
+        [Test]
+        public void NotNull_WithNullObject_ShouldThrowArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => Requires.NotNull(null));
+        }
+
+        [Test]
+        public void NotNull_WithValidObject_ShouldNotThrow()
+        {
+            Assert.DoesNotThrow(() => Requires.NotNull(new object()));
+        }
+
+        [Test]
+        public void NotNullOrEmptyOrWhitespace_WithEmptyString_ShouldThrowArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => Requires.NotNullOrEmptyOrWhitespace(string.Empty));
+        }
+
+        [Test]
+        public void NotNullOrEmptyOrWhitespace_WithValidString_ShouldNotThrow()
+        {
+            Assert.DoesNotThrow(() => Requires.NotNullOrEmptyOrWhitespace("valid string"));
+        }
+
+        [Test]
+        public void NotNullOrEmpty_WithNullIEnumerable_ShouldThrowNullArumentException()
+        {
+            List<object> list = null;
+            Assert.Throws<ArgumentNullException>(() => Requires.NotNullOrEmpty(list));
+        }
+
+        [Test]
+        public void NotNullOrEmpty_WithEmptyIEnumerable_ShouldThrowArumentException()
+        {
+            var list = new List<object>();
+            Assert.Throws<ArgumentException>(() => Requires.NotNullOrEmpty(list));
+        }
+
+        [Test]
+        public void NotNullOrEmpty_WithPopulatedIEnumerable_ShouldNotThrow()
+        {
+            var list = new List<object>() { new object(), new object() };
+            Assert.DoesNotThrow(() => Requires.NotNullOrEmpty(list));
+        }
+    }
+}
