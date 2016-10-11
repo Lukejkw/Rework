638404546775b408080a7948052af68072a2c78a
Luke Warren <lukejkw@gmail.com>
2016-10-11 09:50:22 +0200

    Added range class with before, after and between methods. All tests passing

---
+++ Rework/Range.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rework
+{
+    public static class Range
+    {
+        /// <summary>
+        /// Checks if a date is before another date
+        /// </summary>
+        /// <param name="dateToCheck">The date to use when checking</param>
+        /// <param name="dateToCheckAgainst">The date your want to check against</param>
+        /// <returns></returns>
+        public static bool Before(DateTime dateToCheck, DateTime dateToCheckAgainst)
+        {
+            return dateToCheck < dateToCheckAgainst;
+        }
+
+        /// <summary>
+        /// Checks if a date falls between 2 other dates
+        /// </summary>
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        /// <param name="valueToCheck"></param>
+        /// <returns></returns>
+        public static bool Between(DateTime start, DateTime end, DateTime valueToCheck)
+        {
+            return valueToCheck > start && valueToCheck < end;
+        }
+
+        /// <summary>
+        /// Checks id a double falls between 2 other doubles
+        /// </summary>
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        /// <param name="valueToCheck"></param>
+        /// <returns></returns>
+        public static bool Between(double start, double end, double valueToCheck)
+        {
+            return valueToCheck > start && valueToCheck < end;
+        }
+
+        /// <summary>
+        /// Checks if an int falls between 2 other ints
+        /// </summary>
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        /// <param name="valueToCheck"></param>
+        /// <returns></returns>
+        public static bool Between(int start, int end, int valueToCheck)
+        {
+            return valueToCheck > start && valueToCheck < end;
+        }
+
+        /// <summary>
+        /// Checks if a date falls after another date
+        /// </summary>
+        /// <param name="dateToCheck"></param>
+        /// <param name="dateToCheckAgainst"></param>
+        /// <returns></returns>
+        public static bool After(DateTime dateToCheck, DateTime dateToCheckAgainst)
+        {
+            return dateToCheck > dateToCheckAgainst;
+        }
+    }
+}
---
+++ UnitTests/RangeTests.cs
@@ -0,0 +1,227 @@
+﻿using NUnit.Framework;
+using Rework;
+using System;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class RangeTests
+    {
+        #region Before
+
+        [Test]
+        public void Before_WithDateBefore_ReturnsTrue()
+        {
+            var dateToCheck = new DateTime(2016, 01, 01);
+            var dateToUse = new DateTime(2016, 01, 02);
+            bool result = Range.Before(dateToCheck, dateToUse);
+
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void Before_WithDateAfter_ReturnsFalse()
+        {
+            var dateToCheck = new DateTime(2016, 01, 02);
+            var dateToUse = new DateTime(2016, 01, 01);
+            bool result = Range.Before(dateToCheck, dateToUse);
+
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void Before_WithSameDate_ReturnsFalse()
+        {
+            var dateToCheck = new DateTime(2016, 01, 01);
+            var dateToUse = new DateTime(2016, 01, 01);
+            bool result = Range.Before(dateToCheck, dateToUse);
+
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void Before_WithDateBeforeWithTime_ReturnsTrue()
+        {
+            var dateToCheck = new DateTime(2016, 1, 1, 1, 1, 10);
+            var dateToUse = new DateTime(2016, 1, 2, 1, 1, 1);
+            bool result = Range.Before(dateToCheck, dateToUse);
+
+            Assert.IsTrue(result);
+        }
+
+        #endregion
+
+        #region Between
+
+        [Test]
+        public void Between_WithDateBetween_ReturnsTrue()
+        {
+            bool res = Range.Between(
+                new DateTime(2016, 01, 1),
+                new DateTime(2016, 01, 3),
+                new DateTime(2016, 01, 2));
+
+            Assert.IsTrue(res);
+        }
+
+        [Test]
+        public void Between_WithDateBeforeStart_ReturnsFalse()
+        {
+            bool res = Range.Between(
+                new DateTime(2016, 01, 1),
+                new DateTime(2016, 01, 3),
+                new DateTime(2015, 12, 1));
+
+            Assert.IsFalse(res);
+        }
+
+        [Test]
+        public void Between_WithDateAfterEnd_ReturnsFalse()
+        {
+            bool res = Range.Between(
+                new DateTime(2016, 01, 1),
+                new DateTime(2016, 01, 3),
+                new DateTime(2017, 1, 1));
+
+            Assert.IsFalse(res);
+        }
+
+        [Test]
+        public void Between_WithDateEqualToStart_ReturnsFalse()
+        {
+            bool res = Range.Between(
+                new DateTime(2016, 01, 1),
+                new DateTime(2016, 01, 3),
+                new DateTime(2016, 01, 1));
+
+            Assert.IsFalse(res);
+        }
+
+        [Test]
+        public void Between_WithDateEqualToEnd_ReturnsFalse()
+        {
+            bool res = Range.Between(
+                new DateTime(2016, 01, 1),
+                new DateTime(2016, 01, 3),
+                new DateTime(2016, 01, 3));
+
+            Assert.IsFalse(res);
+        }
+
+        [Test]
+        public void Between_WithIntBetween_ReturnsTrue()
+        {
+            bool res = Range.Between(0, 2, 1);
+
+            Assert.IsTrue(res);
+        }
+
+        [Test]
+        public void Between_WithIntBeforeStart_ReturnsFalse()
+        {
+            bool res = Range.Between(1, 3, 0);
+
+            Assert.IsFalse(res);
+        }
+
+        [Test]
+        public void Between_WithIntAfterEnd_ReturnsFalse()
+        {
+            bool res = Range.Between(1, 2, 3);
+
+            Assert.IsFalse(res);
+        }
+
+        [Test]
+        public void Between_WithIntEqualToStart_ReturnsFalse()
+        {
+            bool res = Range.Between(1, 3, 1);
+
+            Assert.IsFalse(res);
+        }
+
+        [Test]
+        public void Between_WithIntEqualToEnd_ReturnsFalse()
+        {
+            bool res = Range.Between(1, 3, 3);
+
+            Assert.IsFalse(res);
+        }
+
+        [Test]
+        public void Between_WithDoubleBetween_ReturnsTrue()
+        {
+            bool res = Range.Between(0d, 2d, 1d);
+
+            Assert.IsTrue(res);
+        }
+
+        [Test]
+        public void Between_WithDoubleBeforeStart_ReturnsFalse()
+        {
+            bool res = Range.Between(1d, 3d, 0d);
+
+            Assert.IsFalse(res);
+        }
+
+        [Test]
+        public void Between_WithDoubleAfterEnd_ReturnsFalse()
+        {
+            bool res = Range.Between(1d, 2d, 3d);
+
+            Assert.IsFalse(res);
+        }
+
+        [Test]
+        public void Between_WithDoubleEqualToStart_ReturnsFalse()
+        {
+            bool res = Range.Between(1d, 3d, 1d);
+
+            Assert.IsFalse(res);
+        }
+
+        [Test]
+        public void Between_WithDoubleEqualToEnd_ReturnsFalse()
+        {
+            bool res = Range.Between(1d, 3d, 3d);
+
+            Assert.IsFalse(res);
+        }
+
+        #endregion
+
+        #region After
+
+        [Test]
+        public void After_WithDateAfter_ReturnsTrue()
+        {
+            bool res = Range.After(
+                new DateTime(2016, 01, 02),
+                new DateTime(2016, 01, 01));
+
+            Assert.IsTrue(res);
+        }
+
+        [Test]
+        public void After_WithDateBefore_ReturnsFalse()
+        {
+            bool res = Range.After(
+                new DateTime(2016, 01, 01),
+                new DateTime(2016, 01, 02));
+
+            Assert.IsFalse(res);
+        }
+
+        [Test]
+        public void After_WithDateSame_ReturnsFalse()
+        {
+            bool res = Range.After(
+                new DateTime(2016, 01, 01),
+                new DateTime(2016, 01, 01));
+
+            Assert.IsFalse(res);
+        }
+
+        #endregion
+    }
+}
