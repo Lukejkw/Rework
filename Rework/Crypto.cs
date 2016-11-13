4561373a16e11501c84e651ef5a386d13d625eb5
prjseal <prjseal@gmail.com>
2016-11-13 22:44:15 +0000

    Added Crypto class with tests
    Supports SHA1, SHA256 and SHA512

---
+++ Rework/Crypto.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace Rework
+{
+    public static class Crypto
+    {
+        public enum SHA_Type
+        {
+            SHA1 = 0,
+            SHA256 = 1,
+            SHA512 = 2
+        }
+
+        public static string ToSHA(this string inputString, SHA_Type shaType)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(inputString);
+            byte[] hash = GetHash(shaType, bytes);
+            return GetStringFromHash(hash);
+        }
+
+        private static byte[] GetHash(SHA_Type shaType, byte[] bytes)
+        {
+            byte[] hash = null;
+            switch (shaType)
+            {
+                case SHA_Type.SHA1:
+                    SHA1 sha1 = SHA1Managed.Create();
+                    hash = sha1.ComputeHash(bytes);
+                    break;
+                case SHA_Type.SHA256:
+                    SHA256 sha256 = SHA256Managed.Create();
+                    hash = sha256.ComputeHash(bytes);
+                    break;
+                case SHA_Type.SHA512:
+                    SHA512 sha512 = SHA512Managed.Create();
+                    hash = sha512.ComputeHash(bytes);
+                    break;
+                default:
+                    throw new NotSupportedException();
+            }
+
+            return hash;
+        }
+
+        private static string GetStringFromHash(byte[] hash)
+        {
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                result.Append(hash[i].ToString("X2"));
+            }
+            return result.ToString();
+        }
+
+    }
+}
+
+
---
+++ UnitTests/CryptoTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using Rework;
+using System;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class CryptoTests
+    {
+        #region GetRelativeTimeBetweenDates
+
+        //Verified against https://secure.ogone.com/ncol/test/testsha.asp
+
+        [Test]
+        public void ToSHA_SHA1_ShouldMatchValue()
+        {
+            Assert.AreEqual("A9993E364706816ABA3E25717850C26C9CD0D89D", "abc".ToSHA(Crypto.SHA_Type.SHA1));
+        }
+
+        [Test]
+        public void ToSHA_SHA256_ShouldMatchValue()
+        {
+            Assert.AreEqual("BA7816BF8F01CFEA414140DE5DAE2223B00361A396177A9CB410FF61F20015AD", "abc".ToSHA(Crypto.SHA_Type.SHA256));
+        }
+
+        [Test]
+        public void ToSHA_SHA512_ShouldMatchValue()
+        {
+            Assert.AreEqual("DDAF35A193617ABACC417349AE20413112E6FA4E89A97EA20A9EEEE64B55D39A2192992A274FC1A836BA3C23A3FEEBBD454D4423643CE80E2A9AC94FA54CA49F", "abc".ToSHA(Crypto.SHA_Type.SHA512));
+        }
+
+        #endregion
+    }
+}
